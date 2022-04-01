f7293d560198a17546248d47dcd96854862886dc
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2022-04-01 11:39:15 +0530

    second

--- bank_server.c
+++ bank_server.c
@@ -3,6 +3,37 @@
 #include<string.h>
 
 #define QUIT "QUIT"
+#define LOGIN "LOGIN"
+
+int validate(char username[250], char password[250]){
+	FILE *fptr;
+	char line[255];
+	char user_from_file[250];
+	char pass_from_file[250];
+	if ((fptr = fopen("login_file.txt","r")) == NULL){
+    	printf("Error! opening file");
+    	return 1;
+	}
+	while(fscanf(fptr,"%s", line) != EOF){
+		
+	}
+	
+	return 0;
+}
+
+int user_authenticate(){
+	char username[250];
+	char password[250];
+	for(int i=0;i<3;i++){
+		printf("username: ");
+		scanf("%s", username);
+		printf("password: ");
+		scanf("%s", password);
+		if(validate(username, password) == 1)
+			return 1;
+	}
+	return 0;
+}
 
 int quit_server(){
 	return 0;
@@ -15,6 +46,9 @@ int main(){
 		printf("Available commands: LOGIN, QUIT\n");
 		printf("Enter the command: ");
 		scanf("%s", buffer);
+		if(strcmp(buffer, LOGIN) == 0){
+			user_authenticate();
+		}
 	}while(strcmp(buffer, QUIT) != 0);
 	quit_server();
 	return 0;
